7bc647e84c93824e5c98c2da3996cc5dab2b3623
kazudmb <nakano.kazuki.dmb@gmail.com>
2025-10-25 16:25:33 +0900

    refactor: 不要なLambda関連のインポートを削除

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -1,14 +1,4 @@
 import {
-  to = module.backend.module.iam.aws_iam_role.lambda_role
-  id = "${local.project}-lambda-role"
-}
-
-import {
-  to = module.backend.module.iam.aws_iam_policy.ddb_rw
-  id = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/${local.project}-ddb-rw"
-}
-
-import {
   to = module.github_oidc.aws_iam_role.gha_ci[0]
   id = local.gha_role_name
 }
