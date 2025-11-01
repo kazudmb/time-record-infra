8a4841f14576ba5212aa8e6c234ac0364149f245
kazudmb <nakano.kazuki.dmb@gmail.com>
2025-11-01 14:40:32 +0900

    feat: Lambdaモジュールを追加し、APIモジュールとの統合を強化

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -9,7 +9,18 @@ module "iam" {
   table_arn = module.dynamodb.table_arn
 }
 
+module "lambda" {
+  source           = "./lambda"
+  project          = var.project
+  table_name       = module.dynamodb.table_name
+  lambda_role_arn  = module.iam.lambda_role_arn
+  lambda_role_name = module.iam.lambda_role_name
+  lambda_runtime   = var.lambda_runtime
+}
+
 module "api" {
-  source                      = "./api"
-  project                     = var.project
+  source                                   = "./api"
+  project                                  = var.project
+  post_time_record_lambda_invoke_arn       = module.lambda.post_time_record_lambda_invoke_arn
+  post_time_record_lambda_function_name    = module.lambda.post_time_record_lambda_function_name
 }
